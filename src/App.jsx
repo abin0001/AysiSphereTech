6a558ba36bfc26daa93581329bfaa7ff39815f03
abin0001 <abinj2003@gamail.com>
2024-06-14 01:53:09 +0530

    Resposive navbar Added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Navigation from "./components/Navigation"
+
+function App() {
+  return (
+    <div className="App">
+
+   
+      
+        <Navigation>
+
+    </Navigation>
+
+
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Logo from '../img/Logo.png';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+const Navigation = () => {
+  let[open,setOpen]=useState(false);
+  return (
+
+    
+    <div>
+
+      <div className="fixed flex w-screen mx-auto pt-10 rounded-xl 2xl:pr-20 2xl:pl-20 md:pr-10 md:pl-10 h-20 pr-5 pl-5 items-center justify-between">
+        <div>
+          <a href="/"><img src={Logo} className=' w-54 h-16 rounded-2xl font-extrabold  drop-shadow-sm' alt="Logo"/></a>
+        </div>
+
+        <div className={`xl:flex flex-col xl:justify-between md:w-1/3 xl:border-none xl:bg-transparent  p-1 font-bold xl:flex-row rounded-lg absolute xl:static xl:z-auto z-[-1] sm:w-1/2 w-1/3 m-10 border-2 text-center sm:m-10 mt-5 right-0 transition-all duration-500 ${open ? 'top-20':'top-[-490px]'}`}>
+        <div className='xl:hover:scale-110 transition delay-75 rounded-lg p-2 mt-1 text-center xl:hover:text-[#0fff] xl:hover:bg-transparent hover:bg-slate-400 xl:hover:-translate-y-1'><a className=' bg-transparent' href="/">Home</a></div>
+        <div className='xl:hover:scale-110 transition delay-75 rounded-lg p-2 mt-1 text-center xl:hover:text-[#0fff] xl:hover:bg-transparent hover:bg-slate-400 xl:hover:-translate-y-1'><a className=' bg-transparent' href="#about">About</a></div>
+        <div className='xl:hover:scale-110 transition delay-75 rounded-lg p-2 mt-1 text-center xl:hover:text-[#0fff] xl:hover:bg-transparent hover:bg-slate-400 xl:hover:-translate-y-1'><a className=' bg-transparent' href="#services">Our Services</a></div>
+        <div className='xl:hover:scale-110 transition delay-75 rounded-lg p-2 mt-1 text-center xl:hover:text-[#0fff] xl:hover:bg-transparent hover:bg-slate-400 xl:hover:-translate-y-1'><a className=' bg-transparent' href="#works">Our Works</a></div>
+        <div className='xl:hover:scale-110 transition delay-75 rounded-lg p-2 mt-1 text-center xl:hover:text-[#0fff] xl:hover:bg-transparent hover:bg-slate-400 xl:hover:-translate-y-1'><a className=' bg-transparent' href="#contact">Contact Us</a></div>
+          
+        </div>
+        
+        
+        <div className='xl:block hidden'>
+          <button className='shadow-lg shadow-[#0fffff]/50 transition delay-150 bg-[#0fffff] rounded-lg w-20 h-8 font-extrabold text-black hover:-translate-y-1 hover:scale-110 duration-300 '>Join</button>
+        </div>
+
+        <div onClick={()=>setOpen(!open)} className='xl:hidden text-4xl'>
+        <ion-icon name={open ? "close":"menu"}></ion-icon>
+       </div>
+      </div>
+    </div> 
+  )
+}
+export default Navigation
